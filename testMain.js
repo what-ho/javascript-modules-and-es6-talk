a5a2f22b934183b3079264a10d098e56db1dcde6
Alex Pearson <what-ho@users.noreply.github.com>
2015-11-04 16:53:13 +0000

    mocking by redefinition

--- testMain.js
+++ testMain.js
@@ -8,6 +8,15 @@ function mockHello() {
     return 'test hello';
 }
 
+/*Redefine console.log*/
+var actualConsoleLog = console.log;
+
+function mockLog(text) {
+    actualConsoleLog('logged: text');
+}
+
+console.log = mockLog;
+
 var main = proxyquire('./main.js', {
     './module.js' : mockModule
 });
